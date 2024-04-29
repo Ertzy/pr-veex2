9e5e4e8eab5074b880a96ef41847130cc91af343
Ertzy <hertzgaard@gmail.com>
2024-04-29 12:42:43 +0200

    Created backend folder

--- models/visualModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const visualSchema = new mongoose.Schema({
-    value: {type: String},
-})
-
-const visualScreen = mongoose.model('Post', visualSchema);
-
-module.exports = visualScreen;
